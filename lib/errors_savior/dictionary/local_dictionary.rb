43a084bcd76971dc09c36e563bebd01685f04df2
Lucas Voboril <lucas.voboril@wolox.com.ar>
2019-12-18 20:41:46 -0300

    Move dictionary to locale

--- lib/errors_savior/dictionary/local_dictionary.rb
+++ lib/errors_savior/dictionary/local_dictionary.rb
@@ -4,12 +4,13 @@ module ErrorsSavior
   module Dictionary
     module LocalDictionary
       def errors_dictionary
-        dictionary_path = File.join(__dir__, 'local_dictionary.yml')
-        HashWithIndifferentAccess.new(YAML.safe_load(File.read(dictionary_path)))
+        # TODO: Use configurable locale
+        dictionary_path = File.join(__dir__, '..', 'config', 'locale', 'en.yml')
+        HashWithIndifferentAccess.new(YAML.safe_load(File.read(dictionary_path)))[:en]
       end
 
       def required_attributes
-        %i[error_class error_code http_status_sym http_status_code message]
+        %i[error_code http_status_sym http_status_code message]
       end
 
       module_function :errors_dictionary, :required_attributes
--- spec/errors_savior/dictionary/local_dictionary_spec.rb
+++ spec/errors_savior/dictionary/local_dictionary_spec.rb
@@ -2,16 +2,14 @@ describe ErrorsSavior::Dictionary::LocalDictionary do
   describe '.errors_dictionary' do
     subject(:dictionary) { described_class.errors_dictionary }
 
-    let(:model_saviors) do
-      %w[field_validation_error record_invalid record_not_destroyed not_found not_unique]
-    end
-
-    let(:controller_saviors) do
-      %w[parameter_missing]
+    let(:savior_errors) do
+      %w[ActiveModel::ValidationError ActiveRecord::RecordInvalid
+         ActiveRecord::RecordNotDestroyed ActiveRecord::RecordNotFound
+         ActiveRecord::RecordNotUnique ActionController::ParameterMissing]
     end
 
     it 'includes model and controller saviors' do
-      expect(dictionary.keys).to include(*(model_saviors + controller_saviors))
+      expect(dictionary.keys).to include(*savior_errors)
     end
 
     it 'includes the required attributes' do
